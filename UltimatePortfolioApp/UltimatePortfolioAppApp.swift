c8e6ecba74c330a0130d69e949e64c68e8017d0a
Alex Garrett <alex@divergentflow.io>
2025-02-20 11:07:46 -0600

    Initial Commit

---
+++ UltimatePortfolioApp/UltimatePortfolioAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  UltimatePortfolioAppApp.swift
+//  UltimatePortfolioApp
+//
+//  Created by Alex Garrett on 2/20/25.
+//
+
+import SwiftUI
+
+@main
+struct UltimatePortfolioAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
